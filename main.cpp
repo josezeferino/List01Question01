3b0df1742079df93fc0c6f0be0d9285af9baa45d
José Neto <netojz@gmail.com>
2019-06-24 20:04:40 -0300

    Question 01

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+
+ {
+    std::cout << "Hello World!, I'm ready for C++" << std::endl;
+    std::cout << "Hey, write std:: all the time is boring d++" << std::endl;
+    std::cout << "But I know how to avoid this :)" << std::endl;
+    return 0;
+}
